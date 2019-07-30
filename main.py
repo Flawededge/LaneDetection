a0ac4035ec32fc4869dd89a6f274053bcb61fba6
Chris <crdiggle@gmail.com>
2019-07-30 15:39:11 +1200

    Added in a gui. Working on multithreading to allow multiple images to be loaded at once.
    Main.py now only contains gui stuff for now while I develop it and FileLoadTesting.py has the old file load code

--- main.py
+++ main.py
@@ -1,100 +1,52 @@
-""" https # //robotcar-dataset.robots.ox.ac.uk/
-
-    Code to parse the data stored in the oxford robotcar dataset
-
-
-"""
-
-from pathlib import Path
-import loadImages
-import numpy as np
-import cv2 as cv
+import sys
+from PyQt5.QtWidgets import *
+from gui import Ui_MainWindow as window
 import os
-from copy import copy
-from timeit import default_timer as timer  # For benchmarking loading images
-import tkinter
+from PyQt5.QtWidgets import QFileDialog
 
 
-# ---------- User Parameters -------------------------------------------------------------------------------------------
-imageDir = "F:/TestSet/"            # The folder to look for images
-modelDir = "Models/"                # Contains the camera models
-storeDir = f"{imageDir}processed/"  # Relative dir in which to store processed files
-storeTyp = ".jpg"                   # The image format to store processed images in
-scale = 1.0                         # Factor to scale the images before displaying
+# Start of the main UI class. Contains all of the GUI functions
+class Ui(QMainWindow):
+    currentFolder = None
+    directories = []
 
-display = True  # Whether to display the images using imshow from opencv
-waitTime = 1    # If display, then what is the delay between images (0 to wait for key press)
+    def __init__(self):
+        super().__init__()
+        self.ui = window()
+        self.ui.setupUi(self)
+        self.show()
 
-# ---------- Check dir structure ---------------------------------------------------------------------------------------
+        self.directories = [
+            [self.ui.g_f_gps        , "/gps"],
+            [self.ui.g_f_lms_front  , "/lms_front"],
+            [self.ui.g_f_models     , "/Models"],
+            [self.ui.g_f_stereo     , "/stereo"],
+            [self.ui.g_f_left       , "/stereo/left"],
+            [self.ui.g_f_centre     , "/stereo/centre"],
+            [self.ui.g_f_right      , "/stereo/right"],
+            [self.ui.g_f_lmsTime    , "lms_front.timestamps"],
+            [self.ui.g_f_stereoTime , "stereo.timestamps"],
+            [self.ui.g_f_processed, "preprocessed"]  # TODO: Add in preprocessed check
+        ]
 
-# # Get the paths of everything
-views = [[Path(f"{imageDir}stereo/left/"), Path(f"{storeDir}/left")],
-         [Path(f"{imageDir}stereo/centre/"), Path(f"{storeDir}/centre")],
-         [Path(f"{imageDir}stereo/right/"), Path(f"{storeDir}/right")]]
+        self.ui.browseButton.clicked.connect(self.browse_folder)
+        self.ui.folderLine.returnPressed.connect(self.manual_line_change)
 
-gpsP = Path(f"{imageDir}gps/")
-lmsP = Path(f"{imageDir}lms_front/")
 
-error = False
+    def check_folder_structure(self):
+        if not
 
-if not Path(storeDir).is_dir():
-    os.mkdir(storeDir)
+    def browse_folder(self):
+        self.currentFolder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.ui.folderLine.setText(self.currentFolder)
 
-for i in views:
-    if not i[0].is_dir():
-        print(f"Error: '{i}' is not valid path")
-        error = True
+    def manual_line_change(self):
+        print("Enter pressed")
+        self.currentFolder = self.ui.folderLine.text()
 
-    if not i[1].is_dir():
-        os.mkdir(i[1])
 
-if error:
-    print("Error occurred - Exiting")
-    exit()
-else:
-    print("Paths are correct!")
-
-# Get the models of the views which are going to be used
-models = [loadImages.CameraModel(modelDir, str(views[0])),
-          loadImages.CameraModel(modelDir, str(views[1])),
-          loadImages.CameraModel(modelDir, str(views[2]))]
-
-with open(imageDir + "stereo.timestamps") as timestampFile:
-    timestamps = timestampFile.readlines()
-    print(f"Now processing {len(timestamps)} images:")
-    for cnt, line in enumerate(timestamps):
-        start = timer()  # Start timer for benchmarking
-
-        time = line.split()[0]  # Get the line
-        print(f"{cnt}| {time} -> ", end='')
-
-        # Load from image and process it
-        print("Disk -> ", end='')
-        # Load the images
-        images = []
-        for view, model in zip(views, models):
-            cur_view = [copy(view[0]).joinpath(time + ".png"), copy(view[1]).joinpath(time + storeTyp)]
-            if cur_view[1].exists():
-                images.append(cv.imread(str(cur_view[1])))
-            else:  # Load the original image with model and convert it to a cv image
-                images.append((loadImages.load_stereo(str(cur_view[0]), model)))
-
-                # Image is loaded in a 0-255 float, where cv works in 0-1 float, so divide by 255
-                # Also converting the image from a PIL based image to a cv2 based image
-                images[-1] = (np.array(images[-1]))[:, :, ::-1].astype(np.uint8)
-
-                # Store the image in the processed folder
-                cv.imwrite(str(cur_view[1]), images[-1])
-
-        # The image is loaded
-        end = timer()
-        print(f"Loaded in {end-start:.5f}s -> ", end='')
-        # Display the images if applicable
-        if display:
-            cv.imshow("Left", images[0])
-            cv.imshow("Center", images[1])
-            cv.imshow("Right", images[2])
-            print("Image showing -> ", end='')
-            cv.waitKey(waitTime)
-
-        print("Next")
+# Start of the actual main 'loop'. Creates the gui and sits there
+app = QApplication(sys.argv)
+w = Ui()
+w.show()
+sys.exit(app.exec_())
